cb3d5187d1c172144deac463322408e5ed4c3f5f
HeitorCajuhii <cajuhiheitor2244@gmail.com>
2025-07-23 00:24:17 -0300

    formulario alterado, modificações e correções

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 
-document.addEventListener("DOMContentLoaded", function () {
-  const formWpp = document.getElementById("form-whatsapp-envio");
-
-  formWpp.addEventListener("submit", function (e) {
+document.getElementById("form-whatsapp-envio").addEventListener("submit", function (e) {
     e.preventDefault();
 
-    const nome = document.getElementById("nomeContato").value;
-    const email = document.getElementById("emailContato").value;
-    const telefone = document.getElementById("telefoneContato").value;
-    const assunto = document.getElementById("assuntoContato").value;
+    const nome = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+    const telefone = document.getElementById("telefone").value;
+    const assunto = document.getElementById("assunto").value;
     const mensagem = document.getElementById("mensagemContato").value;
 
-    const texto = `Olá, me chamo ${nome}, meu e-mail é ${email}, telefone ${telefone}. Assunto: ${assunto}. Mensagem: ${mensagem}`;
-    const url = `https://wa.me/557199110476?text=${encodeURIComponent(texto)}`;
+    const texto = `Olá, meu nome é *${nome}*.\nAssunto: *${assunto}*\nEmail: ${email}\nTelefone: ${telefone}\nMensagem: ${mensagem}`;
+
+    const telefoneDestino = "557199110476"; // Substitua com o número de destino no formato internacional
+    const url = `https://wa.me/${telefoneDestino}?text=${encodeURIComponent(texto)}`;
 
     window.open(url, "_blank");
   });
-});
 
